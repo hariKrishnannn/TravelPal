a93507e7ecc60fb0184625114fc85c488fefd26b
hariKrishnannn <harikrishnangittt@gmail.com>
2025-08-17 19:48:34 +0530

    added destination model with basic CRUD

--- controller/destinations.js
+++ controller/destinations.js
@@ -1,42 +1,84 @@
-// CONTROLLER METHODS
+const Destination = require('../models/Destination');
 
 // @desc    Get all Destinations
 // @route   GET /api/v1/destinations
 // @access  Public
-exports.getDestinations = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Get All Destinations' });
+exports.getDestinations = async (req, res, next) => {
+  try {
+    const destinations = await Destination.find();
+    res
+      .status(200)
+      .json({ success: true, count: destinations.length, data: destinations });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Get single Destination
 // @route   GET /api/v1/destinations/:id
 // @access  Public
-exports.getDestination = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Get Destination ${req.params.id}` });
+exports.getDestination = async (req, res, next) => {
+  try {
+    const destination = await Destination.findById(req.params.id);
+    if (!destination) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: destination });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Create new Destination
 // @route   POST /api/v1/destinations
 // @access  Private
-exports.createDestination = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new destination' });
+exports.createDestination = async (req, res, next) => {
+  try {
+    const destination = await Destination.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: destination,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Update Destination
 // @route   PUT /api/v1/destinations/:id
 // @access  Private
-exports.updateDestination = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Updated Destination ${req.params.id}` });
+exports.updateDestination = async (req, res, next) => {
+  try {
+    const updatedDestination = await Destination.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!updatedDestination) {
+      res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: updatedDestination });
+  } catch (error) {
+    res.status(400).josn({ success: false });
+  }
 };
 
 // @desc    Delete Destination
 // @route   DELETE /api/v1/destinations/:id
 // @access  Private
-exports.deleteDestination = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete Destination ${req.params.id}` });
+exports.deleteDestination = async (req, res, next) => {
+  try {
+    const updatedDestination = await Destination.findByIdAndDelete(
+      req.params.id
+    );
+    res.status(200).json({ data: updatedDestination });
+    if (!updatedDestination) {
+      res.status(400).json({ success: false });
+    }
+  } catch (error) {
+    res.status(400).josn({ success: false });
+  }
 };
---
+++ models/Destination.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+
+const DestinationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a destination name'],
+    unique: true,
+    trim: true,
+    maxlength: [100, 'Name cannot be more than 100 characters']
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    maxlength: [500, 'Description cannot be more than 500 characters']
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      'Please use a valid URL with HTTP or HTTPS'
+    ]
+  },
+  email: {
+    type: String,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email'
+    ]
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address']
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point']
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere'
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String
+  },
+  attractions: {
+    type: [String],
+    required: true
+  },
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating must be at least 1'],
+    max: [10, 'Rating cannot be more than 10']
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Destination', DestinationSchema);
